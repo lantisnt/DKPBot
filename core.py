0c221196e3b19f4a493521043746eff394f828dd
lantisnt <43088589+lantisnt@users.noreply.github.com>
2020-09-25 20:57:31 +0200

    remove mem check just for now

--- core.py
+++ core.py
@@ -75,7 +75,7 @@ async def discord_attachment_parse(bot, message, normalized_author):
                 }
                 response = bot.BuildDatabase(
                     str(attachment_bytes, 'utf-8'), info)
-                print("Bot for server {0} total footprint: {1} B".format(
+                #print("Bot for server {0} total footprint: {1} B".format(
                     message.guild.name, footprint.total_size(self)))
                 if response.status == dkp_bot.ResponseStatus.SUCCESS:
                     await discord_respond(message.channel, response.data)
