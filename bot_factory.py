842eb298aed6a3ece938f3f0da8175cf35d55391
lantisnt <43088589+lantisnt@users.noreply.github.com>
2020-09-27 18:48:37 +0200

    Fix issues post refactor

--- bot_factory.py
+++ bot_factory.py
@@ -2,15 +2,15 @@ import bot_config
 import essentialdkp_bot
 
 
-def new(guild_id, config):
+def new(guild_id, config: bot_config.BotConfig):
     if not isinstance(config, bot_config.BotConfig):
         return None
 
-    if config.GuildInfo.BotType == 'MonolithDKP':
+    if config.GuildInfo.bot_type == 'MonolithDKP':
         return essentialdkp_bot.EssentialDKPBot(guild_id, config)
-    elif config.GuildInfo.BotType == 'EssentialDKP':
+    elif config.GuildInfo.bot_type == 'EssentialDKP':
         return essentialdkp_bot.EssentialDKPBot(guild_id, config)
-    elif config.GuildInfo.BotType == 'CommunityDKP':
+    elif config.GuildInfo.bot_type == 'CommunityDKP':
         return None
     else:
         return None
