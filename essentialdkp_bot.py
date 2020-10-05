cb41b4ccddc70e2dc3d773f7f36da50f67487eed
lantisnt <43088589+lantisnt@users.noreply.github.com>
2020-10-05 15:31:54 +0200

    Fix typo

--- essentialdkp_bot.py
+++ essentialdkp_bot.py
@@ -296,7 +296,7 @@ class EssentialDKPBot(DKPBot):
                 continue
 
             self._add_loot(player_loot, team)
-            self._add_player_loot(player_loot().player().name(), player_loot, team)
+            self._add_player_loot(player_loot.player().name(), player_loot, team)
 
         self._sort_loot()
         self._sort_player_loot()
