05f7d91bd47d02ca8d844e7c565af8b5bf4733fd
lantisnt <43088589+lantisnt@users.noreply.github.com>
2020-10-08 22:02:24 +0200

    debug

--- dkp_bot.py
+++ dkp_bot.py
@@ -316,6 +316,7 @@ class DKPBot:
         return team_data['history'].get(player.lower())
 
     def _set_dkp(self, player, entry, team):
+        print(team)
         team_data = self.__db['global'].get(team)
         if team_data is None:
             self.__db['global'][team]  = {}
